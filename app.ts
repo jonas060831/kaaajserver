07bf90227a0863775c397bd3d62c9eb485a13b55
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-04-21 00:07:27 -0700

    basic server

---
+++ app.ts
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv'
+import { server } from './src'
+
+// use env variables
+dotenv.config()
+// run the server
+server
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+export const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({message: 'Hello there'});
+});
+
+// Only listen once and export the server
+export const server = app.listen(PORT, () =>  {
+  console.log(`🚀 Server listening on PORT: ${PORT}`);
+});
